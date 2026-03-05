827ec21c972562aec7de8eeed43cfdf336055551
e1356231 <e1356231@u.nus.edu>
2026-03-05 12:25:35 +0800

    Add Rachel's Photo

---
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands;
+
+/**
+ * Adds a remark to an existing person in the address book.
+ */
+public class RemarkCommand extends Command {
+    public static String COMMAND_WORD = "remark";
+}
