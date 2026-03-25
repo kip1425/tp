a499e8f0840c91af1549f2cd8a09a7cdc991fb8d
Raghad <e1375440@u.nus.edu>
2026-03-25 22:13:55 +0800

    Enable equal operator for joindate in filter command

--- src/main/java/seedu/address/logic/parser/CliSyntax.java
+++ src/main/java/seedu/address/logic/parser/CliSyntax.java
@@ -21,4 +21,5 @@ public class CliSyntax {
     public static final Prefix PREFIX_AGE_EQUAL = new Prefix("age=/");
     public static final Prefix PREFIX_JOIN_DATE_AFTER = new Prefix("j>/");
     public static final Prefix PREFIX_JOIN_DATE_BEFORE = new Prefix("j</");
+    public static final Prefix PREFIX_JOIN_DATE_EQUALS = new Prefix("j=/");
 }
